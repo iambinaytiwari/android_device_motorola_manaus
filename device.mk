92fcf121997368fcb173923aa6d815480b294eb2
rexix01 <rexix01@proton.me>
2026-07-04 05:07:51 +0200

    manaus: Switch to OSS AIDL fingerprint service
    * Unify goodix and egis wrapper to udfps extension
    
    Change-Id: I651eb80a7c2d280aae986d5a1d99a7dece443151

--- device.mk
+++ device.mk
@@ -161,8 +161,13 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.manaus \
-    android.hardware.biometrics.fingerprint@2.3-service.manaus2
+    android.hardware.biometrics.fingerprint-service.motorola \
+    libudfpshandler
+
+PRODUCT_PACKAGES += \
+    vendor.egistec.hardware.fingerprint@4.0 \
+    com.motorola.hardware.biometric.fingerprint@1.1 \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
